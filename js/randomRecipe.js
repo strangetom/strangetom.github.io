12067bf25610c19ea6dd6e770734353b7bb8bdf6
Tom Strange <tpstrange@gmail.com>
2018-03-07 18:23:10 +0000

    Add inital website version

---
+++ js/randomRecipe.js
@@ -0,0 +1,8 @@
+function randomRecipe() {
+	var list = document.getElementsByClassName('recipe-list')[0];
+	var rand = Math.floor((Math.random() * list.childElementCount) + 1);
+	var randItem = list.children[rand]
+	randItem.removeAttribute('id');
+	var recipe = document.getElementsByClassName('random-recipe')[0];
+	recipe.innerHTML = randItem.innerHTML;
+}
