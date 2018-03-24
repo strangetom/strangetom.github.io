99051de01a245f3501c6bb0d00aba02291f5832f
Tom Strange <tpstrange@gmail.com>
2018-03-24 11:56:16 +0000

    Reduce fuzzy match threshold so matches need to be more exact

--- js/search.js
+++ js/search.js
@@ -74,7 +74,7 @@ onPatternChange = function() {
 
     var options = {
         shouldSort: true,
-        threshold: 0.6,
+        threshold: 0.22,
         location: 0,
         distance: 100,
         maxPatternLength: 32,
