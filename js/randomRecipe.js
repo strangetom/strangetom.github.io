b53f670b16180fb18546579294bc50cd508fc860
Tom Strange <tpstrange@gmail.com>
2018-10-14 18:18:01 +0100

    Remove random recipe from each category page

--- js/randomRecipe.js
+++
@@ -1,8 +0,0 @@
-function randomRecipe() {
-	var list = document.getElementsByClassName('recipe-list')[0];
-	var rand = Math.floor(Math.random() * (list.childElementCount-1)) + 1;
-	var randItem = list.children[rand]
-	randItem.removeAttribute('id');
-	var recipe = document.getElementsByClassName('random-recipe')[0];
-	recipe.innerHTML = randItem.innerHTML;
-}
